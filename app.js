778bc978ed0224488d0804fb57a63b6d1ff29db1
Tyler Eads <tyler.eads9@gmail.com>
2018-12-18 18:45:44 -0500

    Created SQL Model for Gigs

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-const express = require('express');
-const exphbs = require('express-handlebars');
-const bodyParser = require('body-parser');
-const path = require('path');
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 
+// Database
+const db = require('./config/database');
+
+// Test DB
+db.authenticate()
+    .then(()=> console.log('Database Connected'))
+    .catch(err => console.log("Error :" + err))
+
+//   Initiate Express.JS Application
 const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Index");
+});
+
+// Gig Routes
+app.use('/gigs', require('./routes/gigs'));
 
 const PORT = process.env.PORT || 5000;
 
